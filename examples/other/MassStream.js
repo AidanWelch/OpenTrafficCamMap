e4de34e44faee54ce44e78249c6a274b816fcb2e
Aidan Welch <aidansw@yahoo.com>
2020-08-14 14:49:29 -0400

    MassStream finished

--- examples/other/MassStream.js
+++ examples/other/MassStream.js
@@ -10,25 +10,27 @@ for (var county in cameras.Kentucky) {
     if(!fs.existsSync(`./shots/${county}`)){
         fs.mkdirSync(`./shots/${county}`);
     }
-    (async function(){
+    (async function(county){
         for (var cam of cameras.Kentucky[county]) {
-            http.request(cam.url, (res) => {
-                var data = '';
-                res.setEncoding('binary');
-
-                res.on('error', (err) => {
-                    console.log(cam.url);
-                });
-
-                res.on('data', (chunk) =>{
-                    data += chunk;
-                });
-                
-                res.on('end', () => {
-                    fs.writeFileSync(`./shots/${county}/${cam.location.description.replace(/ /g, '_').replace(/\//g, '∕').replace(/\./g, '')}.jpg`, data, 'binary');
-                });
-            }).setTimeout(10000).end();
-            await new Promise(r => setTimeout(r, 3000));
+            if(cam.url){
+                http.request(cam.url, (res) => {
+                    var data = '';
+                    res.setEncoding('binary');
+    
+                    res.on('error', (err) => {
+                        console.log(cam.url);
+                    });
+    
+                    res.on('data', (chunk) =>{
+                        data += chunk;
+                    });
+                    
+                    res.on('end', () => {
+                        fs.writeFileSync(`./shots/${county}/${cam.location.description.replace(/ /g, '_').replace(/\//g, '∕').replace(/\./g, '')}.jpg`, data, 'binary');
+                    });
+                }).setTimeout(10000).end();
+                await new Promise(r => setTimeout(r, 3000));
+            }
         }
-    })();
+    })(county);
 }
