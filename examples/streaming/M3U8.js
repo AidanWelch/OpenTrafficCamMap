1103a66fb82407b86210c533093f2a5c05854859
Aidan Welch <aidansw@yahoo.com>
2020-08-21 04:05:34 -0400

    implemented really basic M3U8 streaming

--- examples/streaming/M3U8.js
+++ examples/streaming/M3U8.js
@@ -4,28 +4,86 @@ const cameras = JSON.parse(fs.readFileSync('../../cameras/USA.json'));
 
 const test_cam = cameras.Tennessee.Nashville.find(cam => cam.format === 'M3U8');
 
-http.request(test_cam.url, (res) => {
-    var data = '';
+var video_file = fs.createWriteStream(`${test_cam.location.description.replace(/ /g, '_').replace(/\//g, ']').replace(/\./g, '')}.mp4`);
 
-    res.on('data', (chunk) =>{
-        data += chunk;
+(async function(){
+    for(var i = 0; i < 5; i++){
+        await GetPlaylist(test_cam.url);
+        await new Promise((resolve, _) => setTimeout(() => {resolve()}, 14000));
+    }
+    video_file.end();
+})();
+
+function GetPlaylist(url){
+    return new Promise((resolve, reject) => {
+        http.request(url, (res) => {
+            var data = '';
+        
+            res.on('data', (chunk) =>{
+                data += chunk;
+            });
+            
+            res.on('end', () => {
+                GetChunklist(data).then(resolve());
+            });
+
+            res.on('timeout', () => {
+                reject('timeout');
+            });
+        }).end();
     });
-    
-    res.on('end', () => {
-        GetChunklist(data);
-    });
-}).end();
+}
 
 function GetChunklist(data){
-    http.request(test_cam.url.slice(0, -13) + data.slice(data.indexOf('chunklist'), -1), (res) => {
-        var data = '';
-    
-        res.on('data', (chunk) =>{
-            data += chunk;
-        });
+    return new Promise((resolve, reject) => {
+        http.request(test_cam.url.slice(0, -13) + data.slice(data.indexOf('chunklist'), -1), (res) => {
+            var data = '';
         
-        res.on('end', () => {
-            fs.writeFileSync('cam.m3u8', data);
-        });
-    }).end();
+            res.on('data', (chunk) =>{
+                data += chunk;
+            });
+            
+            res.on('end', () => {
+                var lines = data.split('\n');
+                var unloaded_chunks = 0;
+                for (var line of lines){
+                    if(line[0] !== '#'){
+                        unloaded_chunks++;
+                        GetChunk(line).then(() => {
+                            unloaded_chunks--;
+                            if (!unloaded_chunks){
+                                resolve();
+                            }
+                        });
+                    }
+                }
+            });
+
+            res.on('timeout', () => {
+                reject('timeout');
+            });
+        }).end();
+    });
+}
+
+function GetChunk(chunk_name){ 
+    return new Promise((resolve, reject) => {
+        http.request(test_cam.url.slice(0, -13) + chunk_name, (res) => {
+            var data = '';
+            res.setEncoding('binary');
+        
+            res.on('data', (chunk) =>{
+                data += chunk;
+            });
+            
+            res.on('end', () => {
+                video_file.write(data, 'binary');
+                resolve();
+            });
+
+            res.on('timeout', () => {
+                reject('timeout');
+            });
+        }).end();
+    });
 }
