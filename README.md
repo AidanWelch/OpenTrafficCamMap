004ff2289b627d4bc7035eafe3eeeee74921a65a
Aidan Welch <aidansw@yahoo.com>
2020-08-22 17:09:38 -0400

    added camera count

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # OpenTrafficCam
-A crowdsourced database of traffic cameras
+A crowdsourced database of 3076 traffic cameras
 
 ## Camera list
 Cameras will be stored in cameras/[Alpha-3 Country Code].json with the following schema.  All information in brackets should be the relevant input.
---
+++ examples/other/CameraCount.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const cameras = JSON.parse(fs.readFileSync('../../cameras/USA.json'));
+var count = 0;
+for(var state in cameras){
+    for(var county in cameras[state]){
+        for(var camera in cameras[state][county]){
+            count++;
+        }
+    }
+}
+
+console.log(count);
+
+var readme = fs.readFileSync('../../README.md', 'utf8');
+
+readme = readme.split('\n');
+readme[1] = `A crowdsourced database of ${count} traffic cameras`;
+readme = readme.join('\n');
+
+fs.writeFileSync('../../README.md', readme);
