e6a1809ee418a79a4832419ddc3f994f55b639e3
basement <jimspeers.speers@gmail.com>
2022-07-31 14:10:19 -0400

    fix for multi cam single point, add direction when possible

--- compilation/Nebraska511.js
+++ compilation/Nebraska511.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 axios.post('https://new.511.nebraska.gov/api/graphql',
     [
         {
-            'query': 'query ($input: MapFeaturesArgs!) { mapFeaturesQuery(input: $input) { mapFeatures { bbox tooltip uri ...Features __typename ... on Camera { views(limit: 5) { uri url category } } } error { message type } } } fragment Features on FeatureCollection { features { ... on PointFeature { id geometry properties { zIndex clickable icon { url scaledSize { width height } anchor { x y } } popsicle { degrees weight lengthPx color } } } ... on LineFeature { id geometry properties { visible clickable zIndex strokeColor strokeWeight strokeOpacity icons { icon { path scale strokeOpacity strokeColor fillColor fillOpacity } offset repeat } } } ... on AreaFeature { id geometry properties { visible clickable zIndex fillColor fillOpacity strokeColor strokeWeight strokeOpacity } } } }',
+            'query': 'query ($input: MapFeaturesArgs!) { mapFeaturesQuery(input: $input) { mapFeatures { bbox tooltip uri ...Features __typename ... on Camera { views(limit: 5) { uri url category title } } } error { message type } } } fragment Features on FeatureCollection { features { ... on PointFeature { id geometry properties { zIndex clickable icon { url scaledSize { width height } anchor { x y } } popsicle { degrees weight lengthPx color } } } ... on LineFeature { id geometry properties { visible clickable zIndex strokeColor strokeWeight strokeOpacity icons { icon { path scale strokeOpacity strokeColor fillColor fillOpacity } offset repeat } } } ... on AreaFeature { id geometry properties { visible clickable zIndex fillColor fillOpacity strokeColor strokeWeight strokeOpacity } } } }',
             'variables': {
                 'input': {
                     'north': 43.15326372429465,
@@ -33,14 +33,18 @@ axios.post('https://new.511.nebraska.gov/api/graphql',
 class Camera {
     constructor (cam) {
         this.location = {
-            description: cam.tooltip,
+            description: cam.title,
             latitude: cam.bbox[1],
             longitude: cam.bbox[0]
         }
-        this.url = cam.views[0].url;
+        if (cam.direction !== null){
+            this.direction = cam.direction;
+        }
+        this.url = cam.imageUrl;
         this.encoding = "JPEG";
         this.format = "IMAGE_STREAM";
         this.marked_for_review = false;
+        //console.log(this)
     }
 }
 
@@ -50,20 +54,36 @@ function compile(data){
         cameras.Nebraska = {};
     }
     for(var cam of data.mapFeatures){
-        const tooltip = cam.tooltip;
-        if(tooltip !== null && (tooltip.startsWith("I-") || tooltip.startsWith("I ") || tooltip.startsWith("US ") || tooltip.startsWith("NE "))){
-            let urlArr = tooltip.split(":");
-            let region = urlArr[0];
-            region = region.replace(" ", "-");
-            if(!cameras.Nebraska[region]){
-                cameras.Nebraska[region] = [];
+        for(var camView of cam.views)
+        {
+            const tooltip = cam.tooltip;
+            cam.imageUrl = camView.url;
+            cam.title = camView.title;
+
+            let direction = cam.title.substring(cam.title.lastIndexOf(":"), cam.title.length);
+            if (direction.includes("North") || direction.includes("north") || direction.includes("NB" ))
+                cam.direction = 'N';
+            else if (direction.includes("East") || direction.includes("east") || direction.includes("EB"))
+                cam.direction = 'E';
+            else if (direction.includes("South") || direction.includes("south") || direction.includes("SB"))
+                cam.direction = 'S';
+            else if (direction.includes("West") || direction.includes("west") || direction.includes("WB"))
+                cam.direction = 'W';
+
+            if(tooltip !== null && (tooltip.startsWith("I-") || tooltip.startsWith("I ") || tooltip.startsWith("US ") || tooltip.startsWith("NE "))){
+                let urlArr = tooltip.split(":");
+                let region = urlArr[0];
+                region = region.replace(" ", "-");
+                if(!cameras.Nebraska[region]){
+                    cameras.Nebraska[region] = [];
+                }
+                cameras.Nebraska[region].push(new Camera(cam));
+            } else {
+                if(!cameras.Nebraska.other){
+                    cameras.Nebraska.other = [];
+                }
+                cameras.Nebraska.other.push(new Camera(cam));
             }
-            cameras.Nebraska[region].push(new Camera(cam));
-        } else {
-            if(!cameras.Nebraska.other){
-                cameras.Nebraska.other = [];
-            }
-            cameras.Nebraska.other.push(new Camera(cam));
         }
     }
     fs.writeFileSync('../cameras/USA.json', JSON.stringify(cameras, null, 2));
