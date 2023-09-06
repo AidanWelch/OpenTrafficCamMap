1b6031d176388ddac3559a19f7d459cf882da1c2
Aidan Welch <aidan@freedwave.com>
2023-09-06 21:32:28 +0200

    modernize delawareDOT.js

---
+++ compilation/USA/delawareDOT.mjs
@@ -0,0 +1,30 @@
+class Camera {
+	constructor ( cam ) {
+		this.description = cam.title;
+		this.latitude = cam.lat;
+		this.longitude = cam.lon;
+		this.url = cam.urls.m3u8;
+		this.encoding = 'H.264';
+		this.format = 'M3U8';
+		if ( !cam.enabled ) {
+			this.markedForReview = true;
+		}
+	}
+}
+
+async function compile (){
+	const cameras = {};
+	const data = await ( await fetch( 'https://tmc.deldot.gov/json/videocamera.json' ) ).json();
+	for ( const cam of data.videoCameras ){
+		const county = cam.county || 'other';
+		if ( county in cameras === false ){
+			cameras[county] = [];
+		}
+
+		cameras[county].push( new Camera( cam ) );
+	}
+
+	return cameras;
+}
+
+export default [ 'Delaware', compile ];
