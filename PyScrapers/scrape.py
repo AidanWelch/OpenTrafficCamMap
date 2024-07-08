e0fcf64c69174059432be002f078510d935d050d
keyiflerolsun <keyiflerolsun@gmail.com>
2024-07-08 21:51:48 +0300

    🕊
    🕊

--- PyScrapers/scrape.py
+++ PyScrapers/scrape.py
@@ -5,13 +5,13 @@ from asyncio   import run
 from Turkey    import balikesir, marmaris, alanya, trabzon, rize, giresun, erzurum, caycuma
 
 async def basla():
-    # await balikesir.basla()
-    # await marmaris.basla()
-    # await alanya.basla()
-    # await trabzon.basla()
-    # await rize.basla()
-    # await giresun.basla()
-    # await erzurum.basla()
+    await balikesir.basla()
+    await marmaris.basla()
+    await alanya.basla()
+    await trabzon.basla()
+    await rize.basla()
+    await giresun.basla()
+    await erzurum.basla()
     await caycuma.basla()
 
 if __name__ == "__main__":
